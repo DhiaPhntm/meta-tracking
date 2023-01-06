0f900b4a21d45948c11abb7159036d4e31539fef
DhiaPhntm <dhia@phntms.com>
2023-01-06 19:54:22 +0000

    login scope

--- src/components/FBLoginPrompt/index.tsx
+++ src/components/FBLoginPrompt/index.tsx
@@ -1,16 +1,22 @@
 const FBLoginPrompt = () => {
   const login = () => {
     if (window.FB) {
-      window.FB.login(function (response: any) {
-        if (response.authResponse) {
-          console.log("Welcome!  Fetching your information.... ");
-          window.FB.api("/me", function (response: any) {
-            console.log("Good to see you, " + response.name + ".");
-          });
-        } else {
-          console.log("User cancelled login or did not fully authorize.");
+      window.FB.login(
+        function (response: any) {
+          if (response.authResponse) {
+            console.log("Welcome!  Fetching your information.... ");
+            window.FB.api("/me", function (response: any) {
+              console.log("Good to see you, " + response.name + ".");
+            });
+          } else {
+            console.log("User cancelled login or did not fully authorize.");
+          }
+        },
+        {
+          scope:
+            "email, user_age_range, user_birthday, user_gender, user_friends",
         }
-      });
+      );
     }
   };
 
